76aaca60f97351f562fb17c6bb35cdb113cca708
Praveenan Jeevarethinam <praveenanjvp@gmail.com>
2025-07-22 10:14:51 +0530

    Initial backend commit

---
+++ task-service/src/test/java/com/praveenan/task/service/TaskServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praveenan.task.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
